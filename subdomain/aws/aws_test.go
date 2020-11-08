13abd1006168cd23597cdd1e7724c904cda3deaf
David J. Felix <felix.davidj@gmail.com>
2020-11-08 14:58:06 -0500

    test: add ginkgo and gomega to test suite

--- subdomain/aws/aws_test.go
+++ subdomain/aws/aws_test.go
@@ -3,6 +3,9 @@ package aws
 import (
 	"testing"
 
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
@@ -10,38 +13,66 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func Test_DoesCreate(t *testing.T) {
-	// Setup
-	sess, err := session.NewSession(aws.NewConfig())
-	if err != nil {
-		t.Errorf("cannot create session: %s", err)
-	}
+type TestingT struct {
+	GinkgoTInterface
+	desc GinkgoTestDescription
+}
 
-	r53 := route53.New(sess)
-	rootHostedZone, err := r53.CreateHostedZone(&route53.CreateHostedZoneInput{
-		CallerReference: aws.String(random.UniqueId()),
-		Name:            aws.String("0srv.co"),
+func NewTestingT() TestingT {
+	return TestingT{GinkgoT(), CurrentGinkgoTestDescription()}
+}
+
+func (i TestingT) Helper() {
+
+}
+func (i TestingT) Name() string {
+	return i.desc.FullTestText
+}
+
+var _ = Describe("AWS Subdomain", func() {
+	var t TestingT
+
+	BeforeSuite(func() {
+		t = NewTestingT()
 	})
-	if err != nil {
-		t.Errorf("cannot create hosted zone: %s", err)
-	}
-	defer r53.DeleteHostedZone(&route53.DeleteHostedZoneInput{
-		Id: rootHostedZone.HostedZone.Id,
+
+	Context("when a root hosted zone exists", func() {
+		sess, err := session.NewSession(aws.NewConfig())
+		if err != nil {
+			t.Errorf("cannot create session: %s", err)
+		}
+
+		r53 := route53.New(sess)
+		rootHostedZone, err := r53.CreateHostedZone(&route53.CreateHostedZoneInput{
+			CallerReference: aws.String(random.UniqueId()),
+			Name:            aws.String("0srv.co"),
+		})
+		if err != nil {
+			t.Errorf("cannot create hosted zone: %s", err)
+		}
+
+		It("creates expected resources", func() {
+			tfOptions := &terraform.Options{
+				TerraformDir: "./",
+				Vars: map[string]interface{}{
+					"domain_zone":      *rootHostedZone.HostedZone.Name,
+					"name":             "aws.0srv.co subdomain",
+					"should_create":    true,
+					"subdomain_prefix": "aws",
+				},
+			}
+
+			defer terraform.Destroy(t, tfOptions)
+			terraform.InitAndApply(t, tfOptions)
+			defer r53.DeleteHostedZone(&route53.DeleteHostedZoneInput{
+				Id: rootHostedZone.HostedZone.Id,
+			})
+		})
 	})
+})
 
-	// Test
-	tfOptions := &terraform.Options{
-		TerraformDir: "./",
-		Vars: map[string]interface{}{
-			"domain_zone":      *rootHostedZone.HostedZone.Name,
-			"name":             "aws.0srv.co subdomain",
-			"should_create":    true,
-			"subdomain_prefix": "aws",
-		},
-	}
+func TestAWS(t *testing.T) {
 
-	defer terraform.Destroy(t, tfOptions)
-	terraform.InitAndApply(t, tfOptions)
-
-	// Teardown
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "AWS Subdomain Test Suite")
 }
--- subdomain/aws/go.mod
+++ subdomain/aws/go.mod
@@ -5,4 +5,6 @@ go 1.13
 require (
 	github.com/aws/aws-sdk-go v1.27.1
 	github.com/gruntwork-io/terratest v0.30.15
+	github.com/onsi/ginkgo v1.11.0
+	github.com/onsi/gomega v1.7.0
 )
